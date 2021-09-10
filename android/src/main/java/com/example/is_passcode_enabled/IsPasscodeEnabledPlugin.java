74d357152a4c538bbe4f66b50a1cfeace3fa83da
Rachel Bonny <rachel.bonny@koahealth.com>
2021-09-10 17:22:22 +0200

    with android

--- android/src/main/java/com/example/is_passcode_enabled/IsPasscodeEnabledPlugin.java
+++ android/src/main/java/com/example/is_passcode_enabled/IsPasscodeEnabledPlugin.java
@@ -16,17 +16,19 @@ public class IsPasscodeEnabledPlugin implements FlutterPlugin, MethodCallHandler
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private static Context context=null;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "is_passcode_enabled");
     channel.setMethodCallHandler(this);
+    context=flutterPluginBinding.getApplicationContext();
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("isPasscodeEnabled")) {
+      result.success(isDeviceScreenLocked(context));
     } else {
       result.notImplemented();
     }
@@ -36,4 +38,46 @@ public class IsPasscodeEnabledPlugin implements FlutterPlugin, MethodCallHandler
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
   }
+}
+
+
+private static boolean isDeviceScreenLocked(Context appCon) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return isDeviceLocked(appCon);
+    } else {
+      return isPatternSet(appCon) || isPassOrPinSet(appCon);
+    }
+  }
+
+  /**
+   * @return true if pattern set, false if not (or if an issue when checking)
+   */
+  private static boolean isPatternSet(Context appCon) {
+
+    ContentResolver cr = appCon.getContentResolver();
+    try {
+      int lockPatternEnable = Settings.Secure.getInt(cr, Settings.Secure.LOCK_PATTERN_ENABLED);
+      return lockPatternEnable == 1;
+    } catch (Settings.SettingNotFoundException e) {
+      return false;
+    }
+  }
+
+  /**
+   * @return true if pass or pin set
+   */
+  private static boolean isPassOrPinSet(Context appCon) {
+    KeyguardManager keyguardManager = (KeyguardManager) appCon.getSystemService(Context.KEYGUARD_SERVICE); //api 16+
+    return keyguardManager.isKeyguardSecure();
+  }
+
+  /**
+   * @return true if pass or pin or pattern locks screen
+   */
+  @TargetApi(23)
+  private static boolean isDeviceLocked(Context appCon) {
+    KeyguardManager keyguardManager = (KeyguardManager) appCon.getSystemService(Context.KEYGUARD_SERVICE); //api 23+
+    return keyguardManager.isDeviceSecure();
+  }
+
 }
