e0ef373fbc166858507e0bbe1f9802e98d1af610
Rachel Bonny <rachel.bonny@koahealth.com>
2021-09-15 17:01:20 +0200

    attempt

--- lib/is_passcode_enabled.dart
+++ lib/is_passcode_enabled.dart
@@ -6,7 +6,7 @@ class IsPasscodeEnabled {
   static const MethodChannel _channel = const MethodChannel('is_passcode_enabled');
 
   static Future<bool> get isPasscodeEnabled async {
-    final bool isPasscodeEnabled = await _channel.invokeMethod('isPasscodeEnabled');
+    final bool isPasscodeEnabled = await _channel.invokeMethod('is_passcode_enabled');
     return isPasscodeEnabled;
   }
 }
